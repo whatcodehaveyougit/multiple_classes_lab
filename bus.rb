dd16b8ad081a866efb120f2e8ae385d9b66f0860
whatcodehaveyougit <siggywatt@yahoo.com>
2019-11-26 15:31:29 +0000

    2nd

--- bus.rb
+++ bus.rb
@@ -1,14 +1,31 @@
 class Bus
 
-def initialize(route_number, destination)
-	@route_number = route_number
-	@destination = destination
+	def initialize(route_number, destination)
+		@route_number = route_number
+		@destination = destination
+		@passengers = []
+	end
+
+	attr_reader :route_number, :destination
+
+	def drive()
+		return "brum, brum"
+	end	
+
+	def passenger_count
+		return @passengers.length
+	end	
+
+	def pickup(passenger)
+		@passengers.push(passenger)
+	end	
+
+	def drop_off(passenger)
+		@passengers.delete(passenger)
+	end	
+
+	def empty_bus()
+		@passengers.clear
+	end	
 end
 
-attr_reader :route_number, :destination
-
-def drive()
-	return "brum, brum"
-end	
-
-end
--- specs/bus-specs.rb
+++ specs/bus-specs.rb
@@ -4,30 +4,52 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # The more require relatives - the higher up the chain the more require relatives there will be. 
 require_relative('../bus')
+require_relative('../person')
 
 
 class BusTest < MiniTest::Test
 
-def setup()
-	@bus = Bus.new(22, "Ocean Terminal")
+	def setup()
+		@bus = Bus.new(22, "Ocean Terminal")
+		@person = Person.new("Gary", 105)
+		@person_one = Person.new("Sigurd", 106)
 
-end	
+	end	
 
-def test_bus_has_route_number
-	assert_equal(22, @bus.route_number)
-end
+	def test_bus_has_route_number
+		assert_equal(22, @bus.route_number)
+	end
 
-def test_bus_has_destination
-	assert_equal("Ocean Terminal", @bus.destination)
-end
+	def test_bus_has_destination
+		assert_equal("Ocean Terminal", @bus.destination)
+	end
 
-def test_bus_has_drive_method
-	assert_equal("brum, brum", @bus.drive)
-end
+	def test_bus_has_drive_method
+		assert_equal("brum, brum", @bus.drive)
+	end
 
+	def test_passengers_array_starts_empty
+		assert_equal(0, @bus.passenger_count)
+	end
 
+	def test_can_add_passenger_to_bus
+		@bus.pickup(@person)
+		assert_equal(1, @bus.passenger_count)
+	end
 
+	def test_can_remove_passenger_to_bus
+		@bus.pickup(@person)
+		@bus.pickup(@person_one)
+		@bus.drop_off(@person)
+		assert_equal(1, @bus.passenger_count)
+	end
 
+	def test_empty_bus
+		@bus.pickup(@person)
+		@bus.pickup(@person_one)
+		@bus.empty_bus()
+		assert_equal(0, @bus.passenger_count)
+	end	
 
 
 end
